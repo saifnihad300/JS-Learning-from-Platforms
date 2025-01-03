dcc760741aa0b234b10fa54dda8ccd036b3482b3
saifnihad300 <hossainnihad300@gmail.com>
2025-01-04 00:15:13 +0600

    jan3

--- JS complete projects yt/aMERN Application With CRUD, Auth, and Charts Using Refine/client/src/pages/all-properties.tsx
+++ JS complete projects yt/aMERN Application With CRUD, Auth, and Charts Using Refine/client/src/pages/all-properties.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import { CustomButton, PropertyCard } from '../components';
@@ -7,7 +7,15 @@ import { useTable } from '@refinedev/core';
 const AllProperties = () => {
     const navigate = useNavigate();
     
-    const {tableQueryResult: {data, isLoading,  isError} }= useTable();
+    const {
+    tableQueryResult: {data, isLoading,  isError} ,
+     current,
+     setCurrent,
+     setPageSize,
+     pageCount,
+     sorter, setSorters,
+     filters, setFilters
+    } = useTable();
     console.log(data)
 
     const allProperties = data?.data ?? [];
@@ -16,13 +24,51 @@ const AllProperties = () => {
     if(isError) return <Typography>Error...</Typography>
     return (
         <Box>
+            <Box mt='20px' sx = {{display: 'flex', flexWrap: 'wrap', gap: 3}}>
+                <Stack direction="column" width="100%">
+                <Typography fontSize={25} fontWeight={700} color="#11142d">
+                  {!allProperties.length ? 'There are no properties' : 'All Properties'}
+                </Typography>
+
+                <Box mb={2} mt={3} display="flex" justifyContent="space-between" flexWrap="wrap" >
+                    
+                    <Box display="flex" gap={2} flexWrap="wrap" mb={{xs: '20px', sm: 0}}>
+                        <CustomButton
+                        title={`Sort Price`}
+                        //handleClick={ () => {} }
+                        backgroundColor='#476be8'
+                        color='#fcfcfc'
+                        />
+                        <TextField
+                         variant='outlined'
+                         color='info'
+                         placeholder='Search by Title'
+                         value=''
+                         //onChange={()=>{}}
+                        />
+                        <Select
+                         variant='outlined'
+                         color='info'
+                         displayEmpty
+                         required
+                         inputProps={{'aria-label': 'Without label'}}
+                         defaultValue=""
+                         value=""
+                         //onChange={()=>{}}
+                        >
+                        <MenuItem value="" > All</MenuItem>
+                        </Select>
+                    </Box>
+
+                </Box>
+
+                </Stack>
+            </Box>
             <Stack direction="row" 
             justifyContent= "space-between"
             alignItems="center">
             
-            <Typography fontSize={25} fontWeight={700} color="#11142d">
-              Properties
-            </Typography>
+            
              <CustomButton
                 title="Add Property"
                 handleClick={() => navigate("/properties/create")}
@@ -49,6 +95,56 @@ const AllProperties = () => {
             
          
            </Box>
+
+           {allProperties.length>0 && (
+             <Box
+              display="flex"
+              gap = {2}
+              mt = {3}
+              flexWrap="wrap"
+             >
+
+                <CustomButton
+                 
+                 title='Previous'
+                 //handleClick={()=> setCurrent((prev)=prev-1)}
+                 backgroundColor='#474be8'
+                 color='#fcfcfc'
+                 disabled={!(current>1)}
+                />
+
+                <Box display={{ xs: 'hidden', sm: 'flex'}}
+                alignItems="center" gap='5px'>
+                 Page{' '}<strong>{current} of {pageCount}</strong>
+                </Box>
+
+                <CustomButton
+                 
+                 title='Next'
+                 //handleClick={()=> setCurrent((prev)=prev+1)}
+                 backgroundColor='#474be8'
+                 color='#fcfcfc'
+                 disabled={current===pageCount}
+                />
+
+                <Select
+                    variant='outlined'
+                    color='info'
+                    displayEmpty
+                    required
+                    inputProps={{'aria-label': 'Without label'}}
+                    defaultValue="10"
+                    
+                    //onChange={()=>{}}
+                >
+                {[10, 20, 30, 40, 50].map((size) => (
+                    <MenuItem key = {size} value={size} > show {size} </MenuItem>
+                ))}
+                
+                </Select>
+
+             </Box>
+           )}
 
         </Box>
 
